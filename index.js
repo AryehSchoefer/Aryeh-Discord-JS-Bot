46949cf23ac935dc4b2e915701e0023b2f2b0fe2
AryehSchoefer <aryeh.shmuel@googlemail.com>
2020-07-30 03:29:39 +0200

    Added ban and kick command

--- index.js
+++ index.js
@@ -7,9 +7,77 @@ const command = require('./command-handler')
 client.on('ready', () => {
     console.log('Discord Bot is ready!')
 
-    command(client, ['ping', 'p'], message => {
+    command(client, ['ping', 'test'], message => {
         message.channel.send('pong')
     })
+
+    command(client, 'ban', message => {
+        const { member, mentions, guild } = message
+
+        const tag = `<@${member.id}>`
+
+        if (member.hasPermission('ADMINISTRATOR') || member.hasPermission('BAN_MEMBERS')) {
+            const target = mentions.users.first()
+            if (target) {
+                const targetMember = guild.member(target.id)
+                targetMember.ban().catch(err => {
+                    message.channel.send('Unable to ban member')
+                    console.error(err)
+                })
+            } else {
+                message.channel.send(`${tag} Please specify a user.`)
+            }
+        } else {
+            message.channel.send(`${tag} You don't have permission to use this command.`)
+        }
+    })
+
+    // this unban command isn't working yet
+    command(client, 'unban', message => {
+        const { member, mentions, guild } = message
+
+        const tag = `<@${member.id}>`
+
+        if (member.hasPermission('ADMINISTRATOR') || member.hasPermission('BAN_MEMBERS')) {
+            const target = mentions.users.first()
+
+            if (target) {
+                const targetMember = guild.member(target.id)
+                guild.fetchBans().then(bans => {
+                    console.log(bans)
+
+                })
+
+            } else {
+                message.channel.send(`${tag} Please specify a user.`)
+            }
+
+        } else {
+            message.channel.send(`${tag} You don't have permission to use this command.`)
+        }
+    })
+
+    command(client, 'kick', message => {
+        const { member, mentions, guild } = message
+
+        const tag = `<@${member.id}>`
+
+        if (member.hasPermission('ADMINISTRATOR') || member.hasPermission('KICK_MEMBERS')) {
+            const target = mentions.users.first()
+            if (target) {
+                const targetMember = guild.member(target.id)
+                targetMember.kick().catch(err => {
+                    message.channel.send('Unable to kick member')
+                    console.error(err)
+                })
+            } else {
+                message.channel.send(`${tag} Please specify a user.`)
+            }
+        } else {
+            message.channel.send(`${tag} You don't have permission to use this command.`)
+        }
+    })
+
 })
 
 // Basic example: 
